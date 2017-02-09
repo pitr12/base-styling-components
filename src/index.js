2b6f95e61e5a558c9348b4ef4bad9799db4d7779
pitr12 <peter.dubec93@gmail.com>
2017-02-09 09:44:14 +0100

    updates package.json

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import Box from './Box';
 import Text from './Text';
 import defaultTheme from './defaultTheme';
+import type { themeType } from './defaultTheme'; // eslint-disable-line no-duplicate-imports
 
 export { default as Box } from './Box';
 export { default as Text } from './Text';
 export { default as defaultTheme } from './defaultTheme';
+export { default as themeType } from './defaultTheme';
 
 export default {
   Box,
   Text,
   defaultTheme,
+  themeType,
 };
