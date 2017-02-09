a0fc069500f9de51880dba8626b8ae55efd43925
pitr12 <peter.dubec93@gmail.com>
2017-02-09 11:30:17 +0100

    changes default exports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import defaultTheme from './defaultTheme';
 import type { themeType } from './defaultTheme'; // eslint-disable-line no-duplicate-imports
 
 export type { themeType };
+export { default as Box } from './Box';
+export { default as Text } from './Text';
+export { default as defaultTheme } from './defaultTheme';
+
 export default {
   Box,
   Text,
